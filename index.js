010b7d9cd97c371a64917a77ac42b1b957124bfc
laurenwest12 <laurenwest12@gmail.com>
2021-09-29 13:00:32 -0400

    Setup

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { type } = require('./config');
-const { andromedaAuthorization } = require('./authorization');
-const { getStartTime } = require('./functions/getStartTime');
-const { getXlxs, sendErrorReport } = require('./functions/errorReporting');
+const { type } = require("./config");
+const { andromedaAuthorization } = require("./authorization");
+const { getStartTime } = require("./functions/getStartTime");
+const { getXlxs, sendErrorReport } = require("./functions/errorReporting");
 
-app.listen(6000, async () => {
-	console.log('App is listening...');
-	let authorizationResult = await andromedaAuthorization();
+const server = app.listen(6000, async () => {
+  console.log("App is listening...");
+  let authorizationResult = await andromedaAuthorization();
 
-	if (authorizationResult.indexOf('Error') === -1) {
-		console.log('Authorization complete');
-		const lastRunTime = await getStartTime(type);
+  if (authorizationResult.indexOf("Error") === -1) {
+    console.log("Authorization complete");
+    const lastRunTime = await getStartTime(type);
 
-		let allErrors = [];
+    let allErrors = [];
 
-		allErrors = allErrors.flat();
+    allErrors = allErrors.flat();
 
-		if (allErrors.length) {
-			getXlxs(allErrors);
-			await sendErrorReport(type);
-		}
-	}
+    if (allErrors.length) {
+      getXlxs(allErrors);
+      await sendErrorReport(type);
+    }
+  }
+
+  process.kill(process.pid, "SIGTERM");
+});
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
 });
