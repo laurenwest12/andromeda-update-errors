7cd52667767dab1901a101023d02cc12fb798723
laurenwest12 <laurenwest12@gmail.com>
2022-03-21 13:12:55 -0400

    updated insert statement

--- sql.js
+++ sql.js
@@ -8,23 +8,35 @@ const pool = new sql.ConnectionPool({
 	database,
 	trustServerCertificate: true,
 	requestTimeout: 500000,
-})
+});
 
 const connectDb = async () => {
 	try {
-		await pool.connect()
-		return 'Complete'
+		await pool.connect();
+		return 'Complete';
 	} catch (err) {
-		return `Error: ${err?.message}`
+		return `Error: ${err?.message}`;
 	}
-}
-const getSQLServerData = async (table, where) => {
-	const query = `SELECT * FROM ${table} ${where ? where : ''}`
+};
+
+const getLastRunTime = async (program) => {
 	try {
-		const res = await pool.query(query)
-		return res?.recordset
+		const res = await pool.query(
+			`SELECT TOP 1 * FROM AndromedaSchedule WHERE Program = '${program}' ORDER BY LastRunTime DESC`
+		);
+		return res?.recordset;
 	} catch (err) {
-		return `Error: ${err?.message}`
+		return `Error: ${err?.message}`;
+	}
+};
+
+const getSQLServerData = async (table, where) => {
+	const query = `SELECT * FROM ${table} ${where ? where : ''}`;
+	try {
+		const res = await pool.query(query);
+		return res?.recordset;
+	} catch (err) {
+		return `Error: ${err?.message}`;
 	}
 };
 
@@ -34,33 +46,33 @@ const insertTableStatement = (table, fields, values) => {
     FROM (VALUES ${values}) t1 ${fields}`;
 };
 
-const insertStatement = (table, fields, values) => {
-	return `INSERT INTO ${table} SELECT * FROM (VALUES ${values}) t1 ${fields}`;
+const insertStatement = (table, values) => {
+	return `INSERT INTO ${table} VALUES ${values}`;
 };
 
 const executeProcedure = async (proc) => {
 	try {
-		await pool.request().execute(proc)
-		return 'Complete'
+		await pool.request().execute(proc);
+		return 'Complete';
 	} catch (err) {
-		return `Error: ${err?.message}`
-	}
-}
-
-const submitQuery = async (query) => {
-	try {
-		await pool.query(query)
-		return 'Complete'
-	} catch (err) {
-		return `Error: ${err?.message}`
+		return `Error: ${err?.message}`;
 	}
 };
 
-const submitAllQueries = async (fn, data, table, fields) => {
+const submitQuery = async (query) => {
+	try {
+		await pool.query(query);
+		return 'Complete';
+	} catch (err) {
+		return `Error: ${err?.message}`;
+	}
+};
+
+const submitAllQueries = async (fn, data, table) => {
 	const errors = [];
 	for (let i = 0; i < data.length; ++i) {
 		const values = await fn(data[i]);
-		const query = insertStatement(table, fields, values);
+		const query = insertStatement(table, values);
 		const res = await submitQuery(query);
 		if (res.indexOf('Error') !== -1) {
 			errors.push({ query, err: res, type: table });
@@ -71,6 +83,7 @@ const submitAllQueries = async (fn, data, table, fields) => {
 
 module.exports = {
 	connectDb,
+	getLastRunTime,
 	getSQLServerData,
 	executeProcedure,
 	submitQuery,
