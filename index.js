fe9dc0076e85da85866b80acebbebf4ef1883e98
laurenwest12 <laurenwest12@gmail.com>
2022-06-02 16:11:32 -0400

    Made cleaner error handling

--- index.js
+++ index.js
@@ -5,45 +5,30 @@ const { type } = require('./config.js');
 const { andromedaAuthorization } = require('./authorization.js');
 const { getStartTime, submitStartTime } = require('./functions/runTimes.js');
 const { sendErrorReport } = require('./functions/errorReporting.js');
-const { connectDb } = require('./sql')
+const { connectDb } = require('./sql');
 
 const server = app.listen(6000, async () => {
-	console.log('App is listening...');
-	const authorizationResult = await andromedaAuthorization();
-	const connectDbResult = await connectDb()
+  console.log('App is listening...');
+  const errors = [];
+  try {
+    await andromedaAuthorization();
+    await connectDb();
+  } catch (err) {
+    errors.push({
+      type,
+      err: err?.message,
+    });
+  }
 
-	const errors = []
+  if (errors.flat().length) {
+    await sendErrorReport(errors.flat(), type);
+  }
 
-	if (authorizationResult.indexOf('Error') === -1 && connectDbResult === 'Complete') {
-		console.log('Authorization complete');
-		const { lastRunTime, nextRunTime } = await getStartTime(type);
-
-		await submitStartTime(type, nextRunTime);
-	} else {
-		if (authorizationResult.indexOf('Error') !== -1 ) {
-			errors.push({
-				type,
-				err: 'Androemda Authorization Error'
-			})
-		}
-
-		if (connectDbResult !== 'Complete') {
-			errors.push({
-				type,
-				err: 'Connect to DB Error'
-			})
-		}
-	}
-
-	if (errors.flat().length) {
-		await sendErrorReport(errors.flat(), type);
-	}
-
-	process.kill(process.pid, 'SIGTERM');
+  process.kill(process.pid, 'SIGTERM');
 });
 
 process.on('SIGTERM', () => {
-	server.close(() => {
-		console.log('Process terminated');
-	});
+  server.close(() => {
+    console.log('Process terminated');
+  });
 });
--- sql.js
+++ sql.js
@@ -2,90 +2,70 @@ const sql = require('mssql');
 const { server, database, sqlUser, sqlPass } = require('./config.js');
 
 const pool = new sql.ConnectionPool({
-	user: sqlUser,
-	password: sqlPass,
-	server,
-	database,
-	trustServerCertificate: true,
-	requestTimeout: 500000,
+  user: sqlUser,
+  password: sqlPass,
+  server,
+  database,
+  trustServerCertificate: true,
+  requestTimeout: 500000,
 });
 
 const connectDb = async () => {
-	try {
-		await pool.connect();
-		return 'Complete';
-	} catch (err) {
-		return `Error: ${err?.message}`;
-	}
+  await pool.connect();
+  return 'Complete';
 };
 
 const getLastRunTime = async (program) => {
-	try {
-		const res = await pool.query(
-			`SELECT TOP 1 * FROM AndromedaSchedule WHERE Program = '${program}' ORDER BY LastRunTime DESC`
-		);
-		return res?.recordset;
-	} catch (err) {
-		return `Error: ${err?.message}`;
-	}
+  const res = await pool.query(
+    `SELECT TOP 1 * FROM AndromedaSchedule WHERE Program = '${program}' ORDER BY LastRunTime DESC`
+  );
+  return res?.recordset;
 };
 
 const getSQLServerData = async (table, where) => {
-	const query = `SELECT * FROM ${table} ${where ? where : ''}`;
-	try {
-		const res = await pool.query(query);
-		return res?.recordset;
-	} catch (err) {
-		return `Error: ${err?.message}`;
-	}
+  const query = `SELECT * FROM ${table} ${where ? where : ''}`;
+  const res = await pool.query(query);
+  return res?.recordset;
 };
 
 const insertTableStatement = (table, fields, values) => {
-	return `SELECT *
+  return `SELECT *
     INTO ${table}
     FROM (VALUES ${values}) t1 ${fields}`;
 };
 
 const insertStatement = (table, values) => {
-	return `INSERT INTO ${table} VALUES ${values}`;
+  return `INSERT INTO ${table} VALUES ${values}`;
 };
 
 const executeProcedure = async (proc) => {
-	try {
-		await pool.request().execute(proc);
-		return 'Complete';
-	} catch (err) {
-		return `Error: ${err?.message}`;
-	}
+  await pool.request().execute(proc);
+  return 'Complete';
 };
 
 const submitQuery = async (query) => {
-	try {
-		await pool.query(query);
-		return 'Complete';
-	} catch (err) {
-		return `Error: ${err?.message}`;
-	}
+  await pool.query(query);
+  return 'Complete';
 };
 
 const submitAllQueries = async (fn, data, table) => {
-	const errors = [];
-	for (let i = 0; i < data.length; ++i) {
-		const values = await fn(data[i]);
-		const query = insertStatement(table, values);
-		const res = await submitQuery(query);
-		if (res.indexOf('Error') !== -1) {
-			errors.push({ query, err: res, type: table });
-		}
-	}
-	return errors;
+  const errors = [];
+  for (let i = 0; i < data.length; ++i) {
+    const values = await fn(data[i]);
+    const query = insertStatement(table, values);
+    const res = await submitQuery(query);
+    if (res.indexOf('Error') !== -1) {
+      errors.push({ query, err: res, type: table });
+    }
+  }
+  return errors;
 };
 
 module.exports = {
-	connectDb,
-	getLastRunTime,
-	getSQLServerData,
-	executeProcedure,
-	submitQuery,
-	submitAllQueries,
+  connectDb,
+  getLastRunTime,
+  getSQLServerData,
+  executeProcedure,
+  submitQuery,
+  submitAllQueries,
 };
